563b79cc4b342f6861feb08effcdb1f1416d09a8
Chu <ttheko101@gmail.com>
2022-06-27 14:22:05 +0200

    first commit

---
+++ prac-1.py
@@ -0,0 +1,14 @@
+print (type('hello world'))
+
+first_name = "Chu"
+last_name = "Theko"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+print(type(str(int(10)))) #Type Conversion
+
+weather = "It\'s \"kind of\" sunny today " #Escape Sequence "Whatever comes after the backslash is a string"
+#\t adds tab
+#\n new line
+print(weather)

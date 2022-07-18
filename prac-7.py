63f1453f5eae2ba9f454d72ede965b7c1f1daa20
Chu <ttheko101@gmail.com>
2022-07-18 16:28:34 +0200

    added prac-7 file

---
+++ prac-7.py
@@ -0,0 +1,6 @@
+#Scope
+#Scope- What variables do I have access to
+
+#Global scope - anything has access to it 
+#function scope - If the variable is created in a function, it is not apart of the global scope and cannot be accesssed outside the function
+#The onyl time you create a new scope is when you define a function

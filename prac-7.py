6f2b0dc799cfded0a00e83900f43286a2fdf2877
Chu <ttheko101@gmail.com>
2022-07-20 13:52:18 +0200

    lessons 97-113 in Python course

--- prac-7.py
+++ prac-7.py
@@ -1,48 +1,51 @@
-#Scope
-#Scope- What variables do I have access to
+# Scope
+# Scope- What variables do I have access to
 
-#Global scope - anything has access to it 
-#local scope - If the variable is created in a function, it is not apart of the global scope and cannot be accesssed outside the function
-#The onyl time you create a new scope is when you define a function
+# Global scope - anything has access to it
+# local scope - If the variable is created in a function, it is not apart of the global scope and cannot be accesssed outside the function
+# The onyl time you create a new scope is when you define a function
 
-#PYTHON Interperator functioning
+# PYTHON Interperator functioning
 
-#1- start with local scope
-#2 - iS there a parent for the local scope ~ goes up one level
-#3 - Goes to global
-#4 - Built in python functions
+# 1- start with local scope
+# 2 - iS there a parent for the local scope ~ goes up one level
+# 3 - Goes to global
+# 4 - Built in python functions
 
 
-#Parameters are considered local as we can use them inside a function but not outside them
+# Parameters are considered local as we can use them inside a function but not outside them
 
-#Global
-#use the global variable to access a global variable for use in a function
+# Global
+# use the global variable to access a global variable for use in a function
 total = 0
 
+
 def count():
-    global total 
+    global total
     total += 1
     return total
 
 
-#Instead of acessing global variables in this way which may become complicated, use Dependancy Injection
-#Put the variable in tthe parameters
+# Instead of acessing global variables in this way which may become complicated, use Dependancy Injection
+# Put the variable in tthe parameters
 
 def count2(total):
-    total+=1
+    total += 1
     return total
 
-print(count(total))    
 
-#nonlocal
-#Used when you want to use a variable that is not local but also not global ~ For parent local
-#nonlocal keyword, when newly reassigned localy, modifies the original
+print(count(total))
+
+# nonlocal
+# Used when you want to use a variable that is not local but also not global ~ For parent local
+# nonlocal keyword, when newly reassigned localy, modifies the original
 
 
-#Why not have all variables global?
+# Why not have all variables global?
 
-#Computers have limited resources, creating all variables as global can use up a lot of them and cause crashes
-#When creating a variable, the variavle creates a new 'bookshelf' (location in memory)
-#but when modifying it changing it, it doesnt create a new 'bookshelf'
-#once a function is done, the python interperator destroys it in memory ~ gragage collection -
+# Computers have limited resources, creating all variables as global can use up a lot of them and cause crashes
+# When creating a variable, the variavle creates a new 'bookshelf' (location in memory)
+# but when modifying it changing it, it doesnt create a new 'bookshelf'
+# once a function is done, the python interperator destroys it in memory ~ gragage collection -
 #  so that those variables within the function dont take up memory
+print("hello")

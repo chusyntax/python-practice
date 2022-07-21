68c77b3388fd5acf071590bde490ba4c5e7777a4
Chu <ttheko101@gmail.com>
2022-07-21 16:22:58 +0200

    lessons 114-119

---
+++ prac-8.py
@@ -0,0 +1,47 @@
+# OOP
+
+
+# Everything in Python is an object
+# Objects have methods and attributes you xcan access with dot
+# OOP is a paraadigm ~ A way to structure code in a way that is still organised
+# In Python you can create your own data type by using the keyword class ~ First letter is capitalised and uses camel case
+
+class BigObject:
+    pass
+
+
+obj1 = BigObject()
+
+# The class = The blueprint of what we want to create - Basic attributes and properties
+# Classes are then instantiated and create different instances
+
+
+class PlayerCharacter:
+    membership = True  # Class Object attribute
+
+    def __init__(self, name, age):  # Constructor
+        if(self.membership):  # Need to use self.
+            # if(PlayerCharacter.membership): #Can aalso be used like this
+            self.name = name  # For self.name to equal whatever the parameter name is
+            self.age = age
+
+    def run(self):
+        print('run')
+
+    def shout(self):
+        print(f'My name is {self.name}!')
+        # Cannot do PlayerCharacter.name because name is not a class object attribute
+
+# Two underscores = Dunder method
+
+
+player1 = PlayerCharacter()
+
+# FIrst parameter of class should always be self
+
+# Remeber that when a function doesnt returrn anyhing, we get None
+# Class Object Attributes - Static values ~ Used when there will be no change
+# Cllass object attribute is something that doesnt change across different instances ~ Can be accessed without using self.
+# One needs to use self. for function parameters
+
+# __init__ can be used to add different controls and safeguards to make sure we recieve the right data type in order to create the object

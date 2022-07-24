aa7ed9c010c46db380033296adc276724ce87c52
Chu <ttheko101@gmail.com>
2022-07-24 16:21:03 +0200

    added new file prac-10

---
+++ prac-10.py
@@ -0,0 +1,11 @@
+# Super
+
+# If you want to call the __init__ method of a parent class in a child class, use (name of parent ).__init__(self, method you want to call)
+# you can also use super() ~ Refers to the super class/class above
+# With super(), you dont need to pass in self
+# super.__init__(email)
+
+# Introspection
+# The ability to determine the type of an object at runtime
+# Runtime- when the code is running
+# dir() ~ Gives all methods and attributes an instance or class has

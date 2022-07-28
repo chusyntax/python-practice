0e6a762f1922dc1f7c84333da09fd83f00030675
Chu <ttheko101@gmail.com>
2022-07-28 20:38:44 +0200

    lessons 133-136

--- prac-10.py
+++ prac-10.py
@@ -9,3 +9,37 @@
 # The ability to determine the type of an object at runtime
 # Runtime- when the code is running
 # dir() ~ Gives all methods and attributes an instance or class has
+
+# Dunder Methods
+# Inherited from base object class
+# Dunder methods can be modified
+
+# __call__ ~ Allows us to call functions with ()
+
+# Classes can get as many parent classes as they need
+# All you need to do is pass the class the object should inherit in the parameters
+# you need to initialize/inherit  the parent clases functions with their parameters in the subclass (use .__init__)
+
+# Method Resolution Order
+
+class A():
+    num = 10
+
+
+class B(A):
+    pass
+
+
+class C(A):
+    num = 1
+
+
+class D(B, C):
+    pass
+
+
+print(D.num)  # prints out 1
+
+# mro() ~ Methods that shows order of class
+# Checks classes in ascending order
+# MRO uses the algorithm Depth FIrst Search

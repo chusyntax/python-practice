2d3e7af7eaea789969acf7a5e133a7c90b60c7d3
Chu <ttheko101@gmail.com>
2022-08-04 16:01:29 +0200

    lessons 143-144

--- prac-11.py
+++ prac-11.py
@@ -13,6 +13,9 @@
 
 # new_list = [] #Thsi would also be interacting with the outside world
 
+from functools import reduce
+
+
 def multiply_2(li):
     new_list = []
     for item in li:
@@ -65,3 +68,60 @@ print(list(filter(only_odd, [1, 2, 3])))
 
 list_1 = [2, 35, 5]
 list_2 = [5, 7, 1]
+
+print(list(zip(list_1, list_2)))
+
+# Reduce
+
+# Reduces the iterable to a single
+# Not a built in function ~ Must be imported from functools
+
+
+#reduce(function, sequence/data,initial)
+# It accumulates numbers then returns one single number
+
+def accumulator(acc, item):
+    # First item is the first item from the list, acc will default to zero
+    return acc + item
+# For the next pass through, whatever wass returned before will be the acc
+
+
+# We dont need to print with the list method
+print(reduce(accumulator, list_1, 0))
+
+
+# Exercise
+
+# 1 Capitalize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
+
+
+def capitalized(string):
+    return string.upper()
+
+
+print(list(map(capitalized, my_pets)))
+
+
+# 2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5, 4, 3, 2, 1]
+
+my_numbers.sort()
+print(list(zip(my_numbers, my_strings)))
+
+
+# 3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+
+
+def over_50(li):
+    return li > 50
+
+
+print(list(filter(over_50, scores)))
+
+
+# 4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+
+print(reduce(accumulator, (scores + my_numbers)))

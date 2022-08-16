7f2da9843774827206ffcf21990228e1ef78abbb
Chu <ttheko101@gmail.com>
2022-08-16 08:31:57 +0200

    re-organised Python file

--- Python Practice/practice-day-1.py
+++ Python Practice/practice-day-1.py
@@ -36,28 +36,3 @@ for number in range(2000, 3200):
 # print(output)
 
 # Didnt have too much of an idea what was going on except that I needed a loop and the range. Very simple but couldnt figure the logic out
-
-
-# Day 2 - Question 4
-# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.Suppose the following input is supplied to the program:
-
-inp = int(input())
-
-
-def tup_list(inp):
-    inp.list()
-    tuple(inp)
-    return inp
-
-
-tup_list()
-
-# Didnt really figure this one out. Correct solution is doewn below
-# lst = input().split(',')  # the input is being taken as string and as it is string it has a built in
-# method name split. ',' inside split function does split where it finds any ','
-# and save the input as list in lst variable
-
-# tpl = tuple(lst)          # tuple method converts list to tuple
-
-# print(lst)
-# print(tpl)

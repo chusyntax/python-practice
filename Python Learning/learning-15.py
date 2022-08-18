071fbd6dd5d7e97aa0d6d19d43db133be449a1e7
Chu <ttheko101@gmail.com>
2022-08-18 23:56:42 +0200

    lessons 169-172

---
+++ Python Learning/learning-15.py
@@ -0,0 +1,8 @@
+# Modules
+import learning_14
+
+print(learning_14)
+
+# If there are spaces use underscore and nothing in capital (snake case)
+# Use import to import files
+# __main__ is given to the single file we are running

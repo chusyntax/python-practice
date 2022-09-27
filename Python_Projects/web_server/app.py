f19f4e1479f0632e4762badc1ef70a021fbe7680
Chu <ttheko101@gmail.com>
2022-09-27 18:09:03 +0200

    lessons 263-266

--- Python_Projects/web_server/app.py
+++ Python_Projects/web_server/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Help Chu!</p>"
+    return render_template('./index.html')
+
+
+@app.route("/blog")
+def blog():
+    return "<h1>Blog Chu!</h1>"

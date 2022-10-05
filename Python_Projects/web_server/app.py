66c2aa34a1e7fb0937a44c6c5ae55289560fee54
Chu <ttheko101@gmail.com>
2022-10-05 21:56:35 +0200

    lessons 267-269

--- Python_Projects/web_server/app.py
+++ Python_Projects/web_server/app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template('./index.html')
+@app.route("/<username>")
+def hello_world(username=None):
+    return render_template('./index.html', name=username)
 
 
 @app.route("/blog")

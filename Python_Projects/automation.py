d262b87b9d6c5d24c58f76652cc5ca91aced34f6
Chu <ttheko101@gmail.com>
2022-11-07 23:11:01 +0200

    lesson 289

--- Python_Projects/automation.py
+++ Python_Projects/automation.py
@@ -3,3 +3,7 @@ from selenium import webdriver
 chrome_browser = webdriver.Chrome('./chromedriver')
 
 chrome_browser.maximize_window()
+
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+
+print("Selenium Easy Demo" in chrome_browser.title)

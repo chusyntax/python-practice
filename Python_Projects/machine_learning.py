dacbd44237239695ffbe6506f10d92489e58592e
Chu <ttheko101@gmail.com>
2022-12-22 15:09:23 +0200

    lessons 302-303. Also added to Jupyter Notebooks

--- Python_Projects/machine_learning.py
+++ Python_Projects/machine_learning.py
@@ -1,11 +1,6 @@
 # 1 ~ Import the data
-
 # 2 ~ Clean the data
-
 # 3 ~ Split data - Training set/Test set
-
 # 4 ~ Create a model ~ Import the algorithm
-
 # 5 ~ Check the output with the test set
-
 # 6 ~ Improve
